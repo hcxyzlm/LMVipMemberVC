4ce7ce2a186582d5d6f4289ce5756f0d67929e57
zhuolimin <hcxyzlm@126.com>
2016-04-25 17:32:52 +0800

    Initial Commit

---
+++ LMVipMemberVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMVipMemberVC
+//
+//  Created by zhuo on 16/4/25.
+//  Copyright © 2016年 com.lrts.zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
